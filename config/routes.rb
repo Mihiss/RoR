56c0d1d24e2970b9599c1bc99852b9a5572402b9
Mihiss <mihi24@gmail.com>
2016-01-14 01:48:10 +0100

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'visitors#index'
+
   resources :students do
     get :subjects
   end
+
+  resources :teachers do
+    get :subjects
+  end
+
+  get '/report/subjects', to: 'reports#subjects'
+
 end
